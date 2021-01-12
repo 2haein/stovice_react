0b9ef29713b47801dd2b60f015ea7a90e30dc172
hyunin2021 <eehaein2@gmail.com>
2021-01-12 11:28:46 +0900

    상단바 생성

--- src/App.js
+++ src/App.js
@@ -6,26 +6,39 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';v
+import MenuIcon from '@material-ui/icons/Menu';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+function App() {
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 const classes = useStyles();
 
-
-function App() {
   return (
     <div className="App">
+         <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            스토비스
+          </Typography>
+          <Button color="inherit">LOGIN</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
       <header className="App-header">
         <p>
           스토비스 오픈 준비중
@@ -38,19 +51,7 @@ function App() {
         href="https://resume.stovice.com">
       Stovice 창립자 CEO 링크글 참고.
     </Button>
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+ 
       </header>
      
     </div>
