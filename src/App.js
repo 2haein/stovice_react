6ba53cd8a6a1ea617d11bbb86836b94a3434af77
hyunin2021 <eehaein2@gmail.com>
2021-01-12 11:22:33 +0900

    menu bar 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import './App.css';
 import TodoListTemplate from './components/TodoListTemplate';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';v
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const classes = useStyles();
+
 
 function App() {
   return (
@@ -14,9 +35,22 @@ function App() {
       </TodoListTemplate>
        
         <Button variant="contained" color="primary" target="_blank" rel="noopener noreferrer"
-        href="https://hyunin2021.github.io/stovice/">
+        href="https://resume.stovice.com">
       Stovice 창립자 CEO 링크글 참고.
     </Button>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
       </header>
      
     </div>
