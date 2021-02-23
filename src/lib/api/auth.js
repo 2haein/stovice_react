80e579d8ed07c5ec8e7edbbf516fa2c627b45f9a
hyunin2021 <eehaein2@gmail.com>
2021-02-23 10:21:42 +0900

    배포

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -2,14 +2,23 @@ import client from './client';
 
 // 로그인
 export const login = ({ username, password }) =>
-  client.post('/api/auth/login', { username, password });
+  client.post(
+    '/api/auth/login',
+    { username, password },
+    { withCredentials: true },
+  );
 
 // 회원가입
 export const register = ({ username, password }) =>
-  client.post('/api/auth/register', { username, password });
+  client.post(
+    '/api/auth/register',
+    { username, password },
+    { withCredentials: true },
+  );
 
 // 로그인 상태 확인
 export const check = () => client.get('/api/auth/check');
 
 // 로그아웃
-export const logout = () => client.post('/api/auth/logout');
+export const logout = () =>
+  client.post('/api/auth/logout', { withCredentials: true });
--- src/lib/api/posts.js
+++ src/lib/api/posts.js
@@ -2,7 +2,7 @@ import qs from 'qs';
 import client from './client';
 
 export const writePost = ({ title, body, tags }) =>
-  client.post('/api/posts', { title, body, tags });
+  client.post('/api/posts', { title, body, tags }, { withCredentials: true });
 
 export const readPost = (id) => client.get(`/api/posts/${id}`);
 
@@ -16,10 +16,15 @@ export const listPosts = ({ page, username, tag }) => {
 };
 
 export const updatePost = ({ id, title, body, tags }) =>
-  client.patch(`/api/posts/${id}`, {
-    title,
-    body,
-    tags,
-  });
+  client.patch(
+    `/api/posts/${id}`,
+    {
+      title,
+      body,
+      tags,
+    },
+    { withCredentials: true },
+  );
 
-export const removePost = (id) => client.delete(`/api/posts/${id}`);
+export const removePost = (id) =>
+  client.delete(`/api/posts/${id}`, { withCredentials: true });
