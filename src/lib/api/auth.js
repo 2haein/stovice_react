30f5876de906f935e9cf31f123db5864ec5b5383
hyunin2021 <eehaein2@gmail.com>
2021-02-23 12:46:36 +0900

    crendential 추가

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -17,7 +17,8 @@ export const register = ({ username, password }) =>
   );
 
 // 로그인 상태 확인
-export const check = () => client.get('/api/auth/check');
+export const check = () =>
+  client.get('/api/auth/check', { withCredentials: true });
 
 // 로그아웃
 export const logout = () =>
